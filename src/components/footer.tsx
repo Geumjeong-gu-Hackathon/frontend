cfc42e90b9505f6f50a3b796ead43ffdee3e74fe
sebanimm <gks32114221@gmail.com>
2024-08-15 17:19:38 +0900

    푸터 추가

---
+++ src/components/footer.tsx
@@ -0,0 +1,28 @@
+import {
+  CirclePlusIcon,
+  CircleUserRoundIcon,
+  HomeIcon,
+  SearchIcon,
+} from "lucide-react";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 z-50 h-16 w-full border-t-2">
+      <div className="m-auto flex h-full max-w-[480px] items-center justify-between p-2">
+        <Link href="/" className="flex w-20 justify-center">
+          <HomeIcon />
+        </Link>
+        <Link href="" className="flex w-20 justify-center">
+          <SearchIcon />
+        </Link>
+        <Link href="" className="flex w-20 justify-center">
+          <CirclePlusIcon />
+        </Link>
+        <Link href="" className="flex w-20 justify-center">
+          <CircleUserRoundIcon />
+        </Link>
+      </div>
+    </footer>
+  );
+}
